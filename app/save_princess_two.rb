314a00b0790b2936dc97d9fd6d4ab36a5a31e64a
David Bispo Ferreira <davidbispo@hotmail.com>
2021-01-31 23:33:49 -0300

    first draft on save_princess two and testcases on rspec

--- app/save_princess_two.rb
+++ app/save_princess_two.rb
@@ -2,7 +2,6 @@
 class SavePrincessTwo
   class << self
     def nextMove(n,r,c,grid)
-      #
       coordinates = {}
       raw = grid.split("\n")
       board_rows = raw.collect{ |e| e ? e.strip : e }
@@ -16,8 +15,34 @@ class SavePrincessTwo
       player_x, player_y = coordinates['player']
       pr_x, pr_y = coordinates['princess']
 
-      delta_x = pr_x - player_x
-      delta_y = pr_y - player_y
+      delta_x, delta_y = pr_x - [ player_x , pr_y - player_y ]
+      hor_mov_necessary, ver_mov_necessary = [ delta_x != 0, delta_y != 0 ]
+
+      get_directions_x = ->(dx) { dx < 0 ? 'LEFT' : 'RIGHT' }
+      get_directions_y = ->(dy) { dy < 0 ? 'UP' : 'DOWN' }
+
+      if !hor_mov_necessary && ver_mov_necessary
+        # return delta_y < 0 ? print('UP') : print('DOWN')
+        return get_directions_y.call(delta_x)
+
+      end
+      if !ver_mov_necessary && hor_mov_necessary
+        # return delta_x < 0 ? print('LEFT') : print('RIGHT')
+        return get_directions_y.call(delta_y)
+      end
+
+      is_equidistant_from_princess = delta_x == delta_y
+      if is_equidistant_from_princess
+        can_go_to = []
+        can_go_to << get_directions_y.call(delta_x)
+        can_go_to << get_directions_y.call(delta_x)
+        return can_go_to.sample
+      end
+
+      smallest_distance_position = [ delta_x, delta_y ].each_with_index.min[1]
+      return smallest_distance_position == 0 ?
+        get_directions_y.call(delta_x) :
+        get_directions_x.call(delta_y)
     end
   end
 end
--- spec/save_princess/save_princess_2_spec.rb
+++ spec/save_princess/save_princess_2_spec.rb
@@ -3,16 +3,63 @@ require_relative '../../app/save_princess_two.rb'
 
 RSpec.describe SavePrincessTwo do
   let(:n_rows) { 3 }
-  let(:grid) { '-----\n-----\n-----\n----m\n-p---' }
   let(:n) { 5 }
   let(:r) { 2 }
   let(:c) { 3 }
 
   describe '#next_move'
-  context "when a correct input is passed" do
-    it 'Rescues the princess' do
-      result = "LEFT"
-      expect(described_class.nextMove(n,r,c,grid)).to match(result)
+  context "can only move in one direction with an obvious direction" do
+    context "and direction is left" do
+      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+
+      it 'goes left' do
+        result = "LEFT"
+        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+      end
+    end
+    context "and direction is right" do
+      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+      let(:result) { "RIGHT" }
+
+      it 'goes right' do
+        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+      end
+    end
+
+    context "and direction is up" do
+      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+      let(:result) { "UP" }
+
+      it 'goes up' do
+        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+      end
+    end
+    context "and direction is dowm" do
+      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+      let(:result) { "DOWN" }
+
+      it 'Finds the quickest movement' do
+        result = "DOWN"
+        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+      end
+    end
+  end
+  context "can move in multiple directions" do
+    context "and there is a shorter move available" do
+      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+
+      it 'goes to shortest distance movement' do
+        result = "LEFT"
+        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+      end
+    end
+    context "and moves are equidistant in both directions" do
+      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+
+      it 'goes randomly to a direction in the correct sense' do
+        result = "LEFT"
+        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+      end
     end
   end
 end
