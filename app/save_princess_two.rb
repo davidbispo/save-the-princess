045dff8542ef69b70b910992b7aa726811a606df
David Bispo Ferreira <davidbispo@hotmail.com>
2021-02-02 21:47:27 -0300

    first draft on save_princess two. test cases passing

--- app/save_princess_two.rb
+++ app/save_princess_two.rb
@@ -8,14 +8,14 @@ class SavePrincessTwo
       board_rows.each_with_index do |row, row_index|
         princess_col, player_col = [ row.index('p'), row.index('m') ]
 
-        coordinates['player'] = [row_index, player_col] unless !player_col
-        coordinates['princess'] = [row_index, princess_col] unless !princess_col
+        coordinates['player'] = [player_col, row_index] unless !player_col
+        coordinates['princess'] = [ princess_col, row_index ] unless !princess_col
       end
 
       player_x, player_y = coordinates['player']
       pr_x, pr_y = coordinates['princess']
 
-      delta_x, delta_y = pr_x - [ player_x , pr_y - player_y ]
+      delta_x, delta_y =  [ (pr_x - player_x) , (pr_y - player_y) ]
       hor_mov_necessary, ver_mov_necessary = [ delta_x != 0, delta_y != 0 ]
 
       get_directions_x = ->(dx) { dx < 0 ? 'LEFT' : 'RIGHT' }
@@ -23,26 +23,26 @@ class SavePrincessTwo
 
       if !hor_mov_necessary && ver_mov_necessary
         # return delta_y < 0 ? print('UP') : print('DOWN')
-        return get_directions_y.call(delta_x)
-
-      end
-      if !ver_mov_necessary && hor_mov_necessary
-        # return delta_x < 0 ? print('LEFT') : print('RIGHT')
         return get_directions_y.call(delta_y)
       end
 
-      is_equidistant_from_princess = delta_x == delta_y
+      if !ver_mov_necessary && hor_mov_necessary
+        # return delta_x < 0 ? print('LEFT') : print('RIGHT')
+        return get_directions_x.call(delta_x)
+      end
+
+      is_equidistant_from_princess = delta_x.abs == delta_y.abs
       if is_equidistant_from_princess
         can_go_to = []
-        can_go_to << get_directions_y.call(delta_x)
-        can_go_to << get_directions_y.call(delta_x)
+        can_go_to << get_directions_y.call(delta_y)
+        can_go_to << get_directions_x.call(delta_x)
         return can_go_to.sample
       end
 
-      smallest_distance_position = [ delta_x, delta_y ].each_with_index.min[1]
-      return smallest_distance_position == 0 ?
-        get_directions_y.call(delta_x) :
-        get_directions_x.call(delta_y)
+      largest_distance_position = [ delta_x, delta_y ].map(&:abs).each_with_index.max[1]
+      return largest_distance_position == 0 ?
+        get_directions_x.call(delta_x) :
+        get_directions_y.call(delta_y)
     end
   end
 end
--- spec/save_princess/save_princess_2_spec.rb
+++ spec/save_princess/save_princess_2_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe SavePrincessTwo do
       end
     end
     context "and direction is right" do
-      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+      let(:grid) { "-----\n-----\nm--p-\n-----\n-----" }
       let(:result) { "RIGHT" }
 
       it 'goes right' do
@@ -27,38 +27,40 @@ RSpec.describe SavePrincessTwo do
     end
 
     context "and direction is up" do
-      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+      let(:grid) { "-----\n-----\n---p-\n-----\n---m-" }
       let(:result) { "UP" }
 
       it 'goes up' do
         expect(described_class.nextMove(n,r,c,grid)).to match(result)
       end
     end
-    context "and direction is dowm" do
-      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+    context "and direction is down" do
+      let(:grid) { "-----\n-----\n---m-\n-----\n---p-" }
       let(:result) { "DOWN" }
 
-      it 'Finds the quickest movement' do
-        result = "DOWN"
+      it 'goes down' do
         expect(described_class.nextMove(n,r,c,grid)).to match(result)
       end
     end
   end
   context "can move in multiple directions" do
     context "and there is a shorter move available" do
-      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
+      let(:grid) { "----p\n-----\n-----\n-----\n---m-" }
 
-      it 'goes to shortest distance movement' do
-        result = "LEFT"
+      it 'seeks to reduce distance on largest dx axis' do
+        result = "UP"
         expect(described_class.nextMove(n,r,c,grid)).to match(result)
       end
     end
-    context "and moves are equidistant in both directions" do
-      let(:grid) { "-----\n-----\np--m-\n-----\n-----" }
-
-      it 'goes randomly to a direction in the correct sense' do
-        result = "LEFT"
-        expect(described_class.nextMove(n,r,c,grid)).to match(result)
+    context "and dx and dy are equidistant" do
+      let(:grid) { "p----\n-----\n-----\n-----\n----m" }
+      it 'goes to a random direction that makes sense' do
+        results = []
+        (1..50).each do
+          results << described_class.nextMove(n,r,c,grid)
+        end
+        expected = [ "UP" , "LEFT" ]
+        expect(results.uniq).to match_array(expected)
       end
     end
   end
